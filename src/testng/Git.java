c6b9f1fb4065014f8e6446266d2a1d8caac4af5f
TechLearnNandini <163377644+TechLearnNandini@users.noreply.github.com>
2024-03-15 09:58:39 +0530

    Google Launch

--- src/testng/Git.java
+++ src/testng/Git.java
@@ -1,14 +1,20 @@
 package testng;
 
 import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 
 public class Git {
+	 WebDriver driver;
   @Test
-  public void f() {
+  public void Google()
+  {
+	 driver.get("https://google.com/");
   }
   @BeforeTest
   public void beforeTest() {
+	  driver=new ChromeDriver();
   }
 
 }
